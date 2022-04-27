c0f57bda8748b7a3f415b3f98915855d49be1d0b
allancamarena328@gmail.com <>
2022-04-27 09:59:46 -0500

    prueba 1

---
+++ src/pruebagithubcamarena/PruebaGitHubCamarena.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagithubcamarena;
+
+/**
+ *
+ * @author Allan
+ */
+public class PruebaGitHubCamarena {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba Git hub Camarena");
+    }
+    
+}
